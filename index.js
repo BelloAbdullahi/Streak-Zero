c74f2f2421dd3e59aa861b513b9571c996a95e33
BelloAbdullahi <abdullahibello2780@gmail.com>
2025-02-03 18:34:02 +0100

    Hng-streak zero

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
+app.get("/", (req, res) => {
+    res.json({
+      email: process.env.EMAIL,
+      current_datetime: new Date().toISOString(),
+      github_url: "https://github.com/BelloAbdullahi/hng12-api-streak-zero",
+    });
+  });
+
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`server is running on port ${process.env.PORT}`);
+});
